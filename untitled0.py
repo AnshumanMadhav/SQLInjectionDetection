bd90bdbcec9db9fc969b231f02a932c8b1999418
AnshumanMadhav <anshumanmadhav@gmail.com>
2022-02-23 15:23:47 +0530

    make it better

---
+++ untitled0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 22 19:19:49 2022
+
+@author: anshu
+"""
+
+import streamlit as st
+import pickle
+
+def main():
+    st.title("SQL Injection Classifier")
