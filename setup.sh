8101785beaf9257367101dd7d4799e17047fbaf1
Anshuman Madhav <100261013+AnshumanMadhav@users.noreply.github.com>
2022-02-23 19:54:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 23 10:29:52 2022
-
-@author: anshu
-"""
-
 mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port= $PORT\n\
+port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit/comfig.toml
+" > ~/.streamlit/config.toml
