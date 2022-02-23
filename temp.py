023b8c5511871adce97bfb1ff36a76ffdc4afc17
Anshuman Madhav <100261013+AnshumanMadhav@users.noreply.github.com>
2022-02-23 19:27:23 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pickle
+import streamlit as st
+
+model = pickle.load(open("C:/Users/anshu/Downloads/model.pkl","rb"))
+cv = pickle.load(open("C:/Users/anshu/Downloads/count_vectorizer.pkl","rb"))
+
+def main():
+    st.title("SQL Injection Query Detection")
+    st.subheader("Build with Streamlit and Python")
+    msg = st.text_input("Enter Test Query: ")
+    
+    if st.button("Predict"):
+        data = [msg]
+        vect=cv.transform(data).toarray()
+        prediction=model.predict(vect)
+        result = prediction[0]
+        
+        if result == 1:
+            st.error("Mallicious Query")
+        else:
+            st.success("Non-Mallicious Query")
+        
+        
+main()
+
+    
+
+
+    
+    
+
+ 
